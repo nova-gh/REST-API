b083a1a966ab8806d43529d697d76c545d62e178
Nova Gh <51182764+nova-gh@users.noreply.github.com>
2020-12-26 18:43:49 -0800

    delete method

--- app.js
+++ app.js
@@ -108,6 +108,15 @@ app
 				}
 			}
 		);
+	})
+	.delete((req, res) => {
+		Article.deleteOne({ title: req.params.articleTitle }, (err) => {
+			if (!err) {
+				res.send("Sucess: Deleted Article");
+			} else {
+				res.send("Fail: Deleting Article");
+			}
+		});
 	});
 
 app.listen(port, () => {
